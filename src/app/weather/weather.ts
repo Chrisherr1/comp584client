03e20f124cd1579bff168aaadf91b1682338a3a8
ch423002 <ch423002@ECS1538-21>
2025-09-19 17:30:52 -0700

    9/19/25 Update

--- src/app/weather/weather.ts
+++ src/app/weather/weather.ts
@@ -1,16 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { WeatherData } from '../weather-data';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-weather',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './weather.html',
   styleUrl: './weather.scss'
 })
 export class Weather { 
+  forecasts: WeatherData[] = [];
   constructor(http: HttpClient) {
     http.get<WeatherData[]>('http://localhost:5257/weatherforecast').subscribe(result => {
+      this.forecasts = result;
     });
   }
 
